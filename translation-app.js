d284c05028209cf51c2520c07217b412241935cb
rahulyadav139 <ry.rahulmyadav@gmail.com>
2021-10-07 03:24:22 +0530

    translator created

---
+++ translation-app.js
@@ -0,0 +1,23 @@
+var translationInput = document.querySelector("#translate-input");
+var translationOutput = document.querySelector("#translate-output");
+var translationBtn = document.querySelector("#btn-translate");
+
+var serverUrl = "https://api.funtranslations.com/translate/valyrian.json";
+
+function getTranslationUrl(text) {
+    return serverUrl + "?" + "text" + text
+}
+
+function clickHandler() {
+    var inputText = translationInput.value;
+
+    fetch(getTranslationUrl(inputText))
+    .then(responce => responce.json())
+    .then(json => {
+        var translatedText = json.contents.translated;
+        translationOutput.innerText = translatedText;
+    })
+
+}
+
+translationBtn.addEventListener("click", clickHandler)
