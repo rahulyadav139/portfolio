57d533666a0382bfa2f6b39fde9a45cac42e9691
rahulyadav139 <ry.rahulmyadav@gmail.com>
2021-11-29 22:03:27 +0530

    Update portfolio-app.js

--- portfolio-app.js
+++ portfolio-app.js
@@ -2,16 +2,8 @@
 
 const projects = [
   {
-    code: 'p1',
-    projectName: 'My Portfolio',
-    projectDescription:
-      'I am managing my portfolio website with using HtML, CSS, Github and Netlify.',
-    sourceCode: 'https://github.com/rahulyadav139/portfolio',
-    liveLink: '',
-  },
-  {
     code: 'p2',
-    projectName: 'How well do you know me?',
+    projectName: 'Mark 1: How well do you know me?',
     projectDescription:
       'A CLI app built with nodeJS. You can send this to your friends and find out who really knows you.',
     sourceCode:
@@ -20,7 +12,7 @@ const projects = [
   },
   {
     code: 'p3',
-    projectName: 'General knowledge quiz?',
+    projectName: 'Mark 2: General knowledge quiz?',
     projectDescription:
       "How Much Do You Know About India? Let's play a quiz and see.",
     sourceCode:
@@ -28,8 +20,17 @@ const projects = [
     liveLink: 'https://replit.com/@RahulYadav45/Assignment-2?embed=1#index.js',
   },
   {
+    code: 'p1',
+    projectName: 'Mark 3, 4, 5: My Portfolio',
+    projectDescription:
+      'I am managing my portfolio website with using HtML, CSS, Github and Netlify.',
+    sourceCode: 'https://github.com/rahulyadav139/portfolio',
+    liveLink: '',
+  },
+
+  {
     code: 'p4',
-    projectName: 'Minionese',
+    projectName: 'Mark 6: Minionese',
     projectDescription:
       'Are you a fan of minions? Did you know that the gibberish they say is an actual language. Use the translator to convert your text from English to Minion speak or Banana language.',
     sourceCode:
@@ -38,17 +39,8 @@ const projects = [
       'https://rahulmyadav.netlify.app/applications/minion-translator/minion-translator.html',
   },
   {
-    code: 'p5',
-    projectName: 'Country Flag',
-    projectDescription:
-      'This country flag app helps the user to show country flag by tpying the country name or just search in the list.',
-    sourceCode:
-      'https://github.com/rahulyadav139/Assignments/tree/main/country-flag',
-    liveLink: 'https://okwcd.csb.app/',
-  },
-  {
     code: 'p6',
-    projectName: 'Fun Translator',
+    projectName: 'Mark 7: Fun Translator',
     projectDescription:
       'Are you a fan of Game of Thrones? Did you know that the valyrian languages? The Valyrian languages are a fictional language family in the A Song of Ice and Fire series of fantasy novels by George R. R. Martin.',
     sourceCode:
@@ -56,9 +48,10 @@ const projects = [
     liveLink:
       'https://rahulmyadav.netlify.app/applications/fun-translator/translator.html',
   },
+
   {
     code: 'p7',
-    projectName: 'Emoji Interpreter',
+    projectName: 'Mark 8: Emoji Interpreter',
     projectDescription:
       'I struggle with understanding the emojis. Do you too? If yes, then see this library of emojis with meaning.',
     sourceCode:
@@ -67,25 +60,17 @@ const projects = [
   },
   {
     code: 'p8',
-    projectName: 'Recipebook',
+    projectName: 'Mark 9: Recipebook',
     projectDescription:
       'We are always on the hunt for a good restaurant, and we are also on the hunt for easy-cooking, good meals recipes.',
     sourceCode:
       'https://github.com/rahulyadav139/Assignments/tree/main/recipebook',
     liveLink: 'https://yierz.csb.app/',
   },
-  {
-    code: 'p9',
-    projectName: 'Rock Paper Scissors',
-    projectDescription: "let's play a game. You know rock paper scissors game.",
-    sourceCode:
-      'https://github.com/rahulyadav139/portfolio/tree/main/applications/rock-paper-scissors-game',
-    liveLink:
-      'https://rahulmyadav.netlify.app/applications/rock-paper-scissors-game/game.html',
-  },
+
   {
     code: 'p10',
-    projectName: 'Cash Register Manager',
+    projectName: 'Mark 10: Cash Register Manager',
     projectDescription:
       'A cash register manager which helps the user by telling how can he/she return the change to the customer with minimum number of notes.',
     sourceCode:
@@ -95,7 +80,7 @@ const projects = [
   },
   {
     code: 'p11',
-    projectName: 'Is your birthday lucky?',
+    projectName: 'Mark 11: Is your birthday lucky?',
     projectDescription:
       'A website which can help you to check whether your birthday is lucky or not.',
     sourceCode:
@@ -105,12 +90,33 @@ const projects = [
   },
   {
     code: 'p12',
-    projectName: 'Learn Triangles',
+    projectName: 'Mark 12: Learn Triangles',
     projectDescription: 'A website which helps you to learn about triangles.',
     sourceCode:
       'https://github.com/rahulyadav139/portfolio/tree/main/applications/fun-with-triangles',
     liveLink:
       'https://rahulmyadav.netlify.app/applications/fun-with-triangles/angles-to-triangle.html',
+  },
+
+  {
+    code: 'p15',
+    projectName: 'Mark 13: Palindrome Birthdays',
+    projectDescription:
+      'App which checks whether your birthday is a palindrome birthday or not.',
+    sourceCode:
+      'https://github.com/rahulyadav139/portfolio/tree/main/applications/palindrome-birthday',
+    liveLink:
+      'https://rahulmyadav.netlify.app/applications/palindrome-birthday/palindrome.html',
+  },
+  {
+    code: 'p14',
+    projectName: 'Mark 14: Profit Loss Calculator',
+    projectDescription:
+      'A website which helps user how much profit or loss he/she is making in percentage & absolute value.',
+    sourceCode:
+      'https://github.com/rahulyadav139/portfolio/tree/main/applications/profit-loss-calc',
+    liveLink:
+      'https://rahulmyadav.netlify.app/applications/profit-loss-calc/profit-loss-calc.html',
   },
   {
     code: 'p13',
@@ -122,25 +128,24 @@ const projects = [
     liveLink:
       'https://rahulmyadav.netlify.app/applications/leap-year/leap-year.html',
   },
+
   {
-    code: 'p14',
-    projectName: 'Profit Loss Calculator',
-    projectDescription:
-      'A website which helps user how much profit or loss he/she is making in percentage & absolute value.',
+    code: 'p9',
+    projectName: 'Rock Paper Scissors',
+    projectDescription: "let's play a game. You know rock paper scissors game.",
     sourceCode:
-      'https://github.com/rahulyadav139/portfolio/tree/main/applications/profit-loss-calc',
+      'https://github.com/rahulyadav139/portfolio/tree/main/applications/rock-paper-scissors-game',
     liveLink:
-      'https://rahulmyadav.netlify.app/applications/profit-loss-calc/profit-loss-calc.html',
+      'https://rahulmyadav.netlify.app/applications/rock-paper-scissors-game/game.html',
   },
   {
-    code: 'p15',
-    projectName: 'Palindrome Birthdays',
+    code: 'p5',
+    projectName: 'Country Flag',
     projectDescription:
-      'App which checks whether your birthday is a palindrome birthday or not.',
+      'This country flag app helps the user to show country flag by tpying the country name or just search in the list.',
     sourceCode:
-      'https://github.com/rahulyadav139/portfolio/tree/main/applications/palindrome-birthday',
-    liveLink:
-      'https://rahulmyadav.netlify.app/applications/palindrome-birthday/palindrome.html',
+      'https://github.com/rahulyadav139/Assignments/tree/main/country-flag',
+    liveLink: 'https://okwcd.csb.app/',
   },
   {
     code: 'p16',
