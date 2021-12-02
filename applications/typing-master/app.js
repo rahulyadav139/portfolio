6ab98f7feaaebc4d3e22825fe980fc21571a871b
rahulyadav139 <ry.rahulmyadav@gmail.com>
2021-12-02 06:49:14 +0530

    Update app.js

--- applications/typing-master/app.js
+++ applications/typing-master/app.js
@@ -11,13 +11,13 @@ const displayStates = document.querySelectorAll('.info');
 
 //programming
 
-let incorrectWords;
-let correctWords;
+let sampleTextArr;
 let time = 0;
+let timer;
+
 setTimeout(() => {
-  incorrectWords = sampleText.textContent.trim().split(' ');
-  correctWords = sampleText.textContent.trim().split(' ').slice();
-}, 70);
+  sampleTextArr = sampleText.textContent.trim().split(' ');
+}, 80);
 
 document.addEventListener('keydown', e => {
   const selectedKey = keys.find(el => e.keyCode == el.getAttribute('key-code'));
@@ -30,9 +30,9 @@ document.addEventListener('keydown', e => {
 });
 
 window.addEventListener('load', () => {
-  sampleText.textContent = `It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose injected. There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.`;
+  sampleText.textContent = `It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose injected. There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour.`;
 });
-let timer;
+
 function startHandler() {
   if (timer) {
     clearInterval(timer);
@@ -47,25 +47,24 @@ function startHandler() {
 
     displayTimer.textContent = `${min}:${sec}`;
   }, 1000);
+
+  displayStates.forEach(el => (el.style.opacity = 0));
 }
 
 function submitHandler() {
   clearInterval(timer);
-  const inputWordArr = input.value.split(' ');
+  const inputWordsArr = input.value.split(' ');
 
-  for (word of inputWordArr) {
-    let removeIndex1 = incorrectWords.indexOf(word);
-    incorrectWords.splice(removeIndex1, 1);
+  const correctWordsArr = [];
+  for (word of inputWordsArr) {
+    if (sampleTextArr.includes(word)) {
+      correctWordsArr.push(word);
+    }
   }
-  for (word of incorrectWords) {
-    let removeIndex2 = correctWords.indexOf(word);
-    correctWords.splice(removeIndex2, 1);
-  }
-  if (!input.value) correctWords = [];
-  const accuracy =
-    (correctWords.length / (correctWords.length + incorrectWords.length)) * 100;
 
-  const totalCharacters = correctWords.join('').split('').length;
+  const accuracy = (correctWordsArr.length / inputWordsArr.length) * 100;
+
+  const totalCharacters = correctWordsArr.join('').split('').length;
 
   const speed = totalCharacters / 5 / (time / 60);
 
@@ -73,6 +72,7 @@ function submitHandler() {
 
   speedValue.textContent = Math.round(speed);
   accuracyValue.textContent = Math.round(accuracy);
+  input.value = '';
 }
 
 btnStart.addEventListener('click', startHandler);
